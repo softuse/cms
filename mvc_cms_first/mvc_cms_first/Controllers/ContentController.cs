a0983159f0f88184eaf893922b28acb21d87e8b4
Bill <Bill@hotmail.com>
2018-11-22 15:40:39 +0800

    jiaruproject

---
+++ mvc_cms_first/mvc_cms_first/Controllers/ContentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvc_cms_first.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace mvc_cms_first.Controllers
+{
+    /// <summary>
+    /// 处理逻辑
+    /// </summary>
+    public class ContentController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var contents = new List<Content>();
+            for (int i = 1; i < 10; i++)
+            {
+                contents.Add(new Content { ID = i, Title = $"{i}的标题", Contents = $"{i}的内容", Status = 1, Add_time = DateTime.Now.AddDays(-i) });
+
+            }
+            return View(new ContentViewModel { Contents = contents });
+
+        }
+    }
+}
---
+++ mvc_cms_first/mvc_cms_first/Models/Content.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_cms_first.Models
+{
+    public class Content
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Contents { get; set; }
+
+        public int Status { get; set; }
+
+        public DateTime Add_time { get; set; }
+
+        public DateTime Modify_time { get; set; }
+    }
+}
